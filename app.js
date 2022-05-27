5f09b8df4b9e4452d4a95f906a94be8ee9f778f9
olarag <oscar@difusion.mx>
2022-05-27 14:09:08 -0500

    se agregó ruta de explorersInNode

--- app.js
+++ app.js
@@ -13,3 +13,8 @@ app.listen(port, () => {
 app.get('/launchx', (req, res) =>{
 	res.send('Bienvenidos a launchx')
 })
+
+app.get('/explorersInNode' (req,res) => {
+	const explorer = {name: "Explorer", msg: "Hello"}
+	res.send(explorer)
+})
